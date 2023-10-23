26d9826c968112d42356b15c99867ac37e7db743
Michal Ďuračík <michal@duracik.sk>
2023-10-23 08:49:32 +0200

    Faktoria

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
+declare(strict_types=1);
 
-
+function faktorial(int $n): int
+{
+    if ($n < 2) {
+        return 1;
+    }
+    return $n * faktorial($n - 1);
+}
 echo "Ahoj svet!";
 
 echo "<br>";
@@ -8,3 +15,10 @@ echo "<br>";
 for ($i = 1; $i <= 10; $i++) {
     echo "$i<br>\n";
 }
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "$i! = " . faktorial($i) . "<br>";
+}
+
+
+echo faktorial(3.14);
