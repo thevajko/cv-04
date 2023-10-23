6635116cef742861687a439f2d6bc706dadc204b
hrkut <hrkut@kst.uniza.sk>
2023-10-23 18:47:27 +0200

    Faktorial

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
+declare(strict_types=1);
 
-for ($i = 0; $i < 11; $i++) {
-    echo "$i<br>";
+function faktorial(int $f) : int
+{
+    $fkt = 1;
+    for ($i = 1; $i <= $f; $i++) {
+        $fkt *= $i;
+    }
+    return $fkt;
+}
+
+
+for ($i = 1; $i < 10; $i++) {
+    echo "$i!= " . faktorial($i) . "<br>";
 }
