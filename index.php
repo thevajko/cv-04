67157a08b389eb4fbfc662224737390364c7416f
Matej Meško <meshosk@gmail.com>
2023-10-23 10:38:26 +0200

    doplneny faktoriak

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 
+function faktorial($cislo) {
+    if ($cislo < 1) {
+        return 1;
+    } else {
+        return $cislo * faktorial($cislo -1);
+    }
+}
+
 for ($i = 1; $i < 11; $i++) {
-    echo $i."<br>\n";
+    echo "!".$i."=".faktorial($i)."<br>\n";
 }
 
 
