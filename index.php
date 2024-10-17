472f880027c8f12738cfbb74cd0c4457f135b257
Matej Meško <meshosk@gmail.com>
2024-10-17 12:37:19 +0200

    Pridanie externeho suboru pre vypocet faktorialu

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
+    include  "faktorial.php";
+?><html>
+<head></head>
+<body>
+<?php
 
 for ($i = 0; $i < 5; $i++) {
-    echo "<div>".$i. "</div>";
+    echo "<div>" .$i. "</div>";
 }
 
+?></body>
+</html>
