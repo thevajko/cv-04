8e524c467ad467f9c3ad4f592177ae5bd3e1821f
Michal Ďuračík <michal@duracik.sk>
2025-10-20 17:54:24 +0200

    Uloha 1

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
 echo "Ahoj svet!";
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "\nCislo: " . $i . "</br>";
+}
+
+echo "<hr>";
+$n = 104;
+echo "Faktorial cisla $n je: " . faktorial($n);
+
+function faktorial(int $n)
+{
+    if ($n <= 1) {
+        return 1;
+    } else {
+        return $n * faktorial($n - 1);
+    }
+}
