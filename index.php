3cfc9006abe5b157fc91802f47e9d6ca364a0f2f
Michal Ďuračík <michal@duracik.sk>
2025-10-22 11:45:29 +0200

    Pridanie funkcie na výpočet faktoriálu a jej zobrazenie

--- index.php
+++ index.php
@@ -4,3 +4,13 @@ echo "Ahoj svet!\n";
 for ($i = 0; $i < 10; $i++) {
     echo "Cislo: " . $i . "<br>\n";
 }
+
+function faktorial(int $n) : int {
+    if ($n <= 1) {
+        return 1;
+    }
+    return $n * faktorial($n - 1);
+}
+
+$n = 20;
+echo "Faktorial $n je: " . faktorial($n) . "\n";
